5ee44a03c1eb47f2904ac6759c964eb0569c00e8
caorpr <caorpr@gmail.com>
2015-09-24 12:28:30 -0600

    PopupProject

---
+++ src/input/controller/PopupRunner.java
@@ -0,0 +1,14 @@
+package input.controller;
+
+public class PopupRunner 
+{
+	public static void main (String [] args)
+	{
+		//Something mySomething = newSomething();
+		//mySomething.start();
+		PopupController myPopupController = new PopupController();
+		myPopupController.start();
+	}
+	
+
+}
