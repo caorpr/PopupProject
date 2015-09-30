2593006d48ebebe2987c25b69c47b79642489c67
caorpr <caorpr@gmail.com>
2015-09-30 13:20:04 -0600

    added the loops to see if you are typing in correct

--- src/input/controller/PopupController.java
+++ src/input/controller/PopupController.java
@@ -3,17 +3,29 @@ package input.controller;
 import input.view.PopupDisplay;
 import input.model.Thing;
 
-
+/**
+ * Makes the popup display usable in other areas.
+ * @author cand3952
+ */
 public class PopupController 
 {
+	
 	private PopupDisplay myPopups;
 	private Thing myThing;
 	
+/**
+ * Makes it so that there is a popup window.
+ */
 	public PopupController()
 	{
 		myPopups = new PopupDisplay();
 	}
-
+/**
+ * Asks questions, and has you type in answers for age, name, and weight. Tests to see if... 
+ * ...you typed in a double for weight, and an integer for age. If not, it will ask you to type in again.
+ * After you type, it will show what you typed (or give you an error).
+ */
+	
 	public void start()
 	{
 		String name = myPopups.getAnswer("Type in your name: ");
@@ -60,6 +72,11 @@ public class PopupController
 		myPopups.displayResponse(myThing.toString());
 	}
 
+/**
+ * Sees if the integer for age is true, otherwise, it will loop back and give you an error.
+ * @param input (put in an integer, or it will give you an error!)
+ * @return
+ */
 	private boolean isInteger(String input)
 	{
 		boolean isInt = false;
@@ -78,6 +95,11 @@ public class PopupController
 		return isInt;
 	}
 	
+/**
+ * Sees if the double for weight is true, otherwise, it will loop an error message until you give it a double.
+ * @param input (put in a double!)
+ * @return
+ */
 	private boolean isDouble(String input)
 	{
 		boolean isDouble = false;
--- src/input/model/Thing.java
+++ src/input/model/Thing.java
@@ -6,6 +6,10 @@ public class Thing
 	private int age;
 	private double weight;
 	
+	/**
+	 * Makes the variables exist.
+	 */
+	
 	public Thing()
 	{
 		age = -99;
@@ -13,12 +17,60 @@ public class Thing
 		name = "";
 	}
 	
+	/**
+	 * makes the variables able to be changed.
+	 * @param name (sets name to user typed out)
+	 * @param (age sets age to what the user types out)
+	 * @param (weight sets weight to what was typed out)
+	 */
+	
 	public Thing(String name, int age, double weight)
 	{
 		this.age = age;
 		this.name = name;
 		this.weight = weight;
 	}
+	
+
+	
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getAge()
+	{
+		return age;
+	}
+	
+	public double getWeight()
+	{
+		return weight;
+	}
+	
+	
+	
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+	
+	public void setWeight(double weight)
+	{
+		this.weight = weight;
+	}
+	
+	/**
+	 * Returns a STRING, and describes what they are.
+	 */
+	
 	
 	public String toString()
 	{
